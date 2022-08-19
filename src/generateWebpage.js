d1df788a4277c0aea41d67f71baed8a39db36c8c
anthony3738-miZJgd <anthony3738@gmail.com>
2022-08-18 22:39:33 -0700

    Re-factored code to include use of classes

--- src/generateWebpage.js
+++ src/generateWebpage.js
@@ -1,5 +1,9 @@
 // This module is utilized to generate all the HTML web elements to display the team information
 
+const Manager = require('../lib/Manager');
+const Intern = require('../lib/Intern');
+const Engineer = require('../lib/Engineer');
+
 // Temporary data setup to ease troubleshooting and code development
 const data = {
     managerName: 'Manager',
@@ -7,62 +11,78 @@ const data = {
     managerEmail: 'manager@manager.com',
     managerNumber: '123456789',
     employees: [
-      {
-        employeeRole: 'Engineer',
-        employeeName: 'Engineer',
-        employeeID: '123',
-        employeeEmail: 'eng@eng.com',
-        engGithub: 'Engineer'
-      },
-      {
-        employeeRole: 'Intern',
-        employeeName: 'Intern',
-        employeeID: '1',
-        employeeEmail: 'int@int.com',
-        internSchool: 'internUniversity'
-      }
+        {
+            employeeRole: 'Engineer',
+            employeeName: 'Engineer',
+            employeeID: '123',
+            employeeEmail: 'eng@eng.com',
+            engGithub: 'Engineer'
+        },
+        {
+            employeeRole: 'Intern',
+            employeeName: 'Intern',
+            employeeID: '1',
+            employeeEmail: 'int@int.com',
+            internSchool: 'internUniversity'
+        },
+        {
+            employeeRole: 'Intern',
+            employeeName: 'Intern',
+            employeeID: '1',
+            employeeEmail: 'int@int.com',
+            internSchool: 'internUniversity'
+        },
+        {
+            employeeRole: 'Engineer',
+            employeeName: 'Engineer',
+            employeeID: '123',
+            employeeEmail: 'eng@eng.com',
+            engGithub: 'Engineer'
+        },
     ]
-  }
+}
 
-
+renderEmployees(data)
 
 // The renderEmployees function generates the HTML code for the employees reporting to the manager 
 function renderEmployees(data) {
-    const { employees } = data;
+    const staff = data.employees;
+    for (let i = 0; i < staff.length; i++) {
 
-    if (employees.length !== 0) {
+        const currentStaff = staff[i];
+        if (currentStaff.employeeRole == 'Engineer') {
+            staff[i] = new Engineer(currentStaff.employeeName, currentStaff.employeeID, currentStaff.employeeEmail, currentStaff.engGithub);
+        } else {
+            staff[i] = new Intern(currentStaff.employeeName, currentStaff.employeeID, currentStaff.employeeEmail, currentStaff.internSchool)
+        }
+    }
+
+    if (staff.length !== 0) {
         let employeeSection = '';
+        let uniqueEmployeeInfo = '';
 
-        for (let i = 0; i < employees.length; i++) {
-            let { employeeRole, employeeName, employeeID, employeeEmail, engGithub, internSchool } = employees[i];
-            let uniqueEmployeeInfo
-            // console.log(employeeRole)
-            // let uniqueEmployeeInfo = employeeRole = 'Engineer' ? `GitHub: <a href=https://www.github.com/${engGithub}>${engGithub}</a>` : `School: ${internSchool}`
-            
-            if (employeeRole =='Engineer') {
-                uniqueEmployeeInfo = `GitHub: <a href=https://www.github.com/${engGithub}>${engGithub}</a>`
-            } else {
-                uniqueEmployeeInfo = `School: ${internSchool}`
-            };
-
+        staff.forEach((item) => {
+            uniqueEmployeeInfo = item.getRole() == 'Engineer' ? `GitHub: <a href=https://www.github.com/${item.getGithub()}>${item.getGithub()}</a>` : `School: ${item.getSchool()}`
 
             let employeeHTML = `
             <div class="card" style="width: 18rem;">
                 <div class="card-title">
-                    <h5>${employeeName}</h5>
-                    <h4>${employeeRole}</h4>
+                    <h5>${item.name}</h5>
+                    <h4>${item.role}</h4>
                 </div>
                 <div class="card-body">
                     <h6 class="card-subtitle mb-2 text-muted">Employee Information:</h6>
                     <ul>
-                        <li class="card-text">ID: ${employeeID}</li>
-                        <li class="card-text">Email: ${employeeEmail}</li>
+                        <li class="card-text">ID: ${item.ID}</li>
+                        <li class="card-text">Email: ${item.email}</li>
                         <li class="card-text">${uniqueEmployeeInfo}</li>
                     </ul>
                 </div>
                 </div>`
             employeeSection += employeeHTML
-        };
+        })
+
+
         return employeeSection
     } else {
         return ''
@@ -101,6 +121,9 @@ function generateWebpage(data) {
         <hr class="my-4">
         <p>We're united by our love for coding!</p>
     </div>
+
+    ${employeeHTML}
+    ${managerHTML}
         
     </body>
     </html>
